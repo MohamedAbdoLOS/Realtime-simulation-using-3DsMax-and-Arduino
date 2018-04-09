b018fe5337e074d3f1fbc248fe2b27c453263e27
Mohamed Abdo <moha_the.best@hotmail.com>
2018-04-09 21:01:46 +0200

    Add files via upload

---
+++ SerialTest.ino
@@ -0,0 +1,23 @@
+
+void setup()
+{
+ Serial.begin(9600);
+ pinMode(A3, INPUT);
+ pinMode(A4, INPUT);
+ pinMode(A5, INPUT);
+}
+
+void loop()
+{   
+  int detct = analogRead(A5);
+  int x_byte = analogRead(A3); 
+  int y_byte = analogRead(A4);
+  Serial.print(x_byte); 
+  Serial.print(",");
+  Serial.print(y_byte);
+  Serial.print(",");
+  Serial.println(detct);
+  delay(300);
+}
+
+
